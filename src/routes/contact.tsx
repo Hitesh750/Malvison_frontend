56b0f7d06c043d5380fb26aa9778e0d1c031e3a2
Hitesh750 <hitumalviya2000@gmail.com>
2026-05-30 08:06:35 -0700

    new code of frontend

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import { Mail, MapPin, Phone, Send, CheckCircle2 } from "lucide-react";
 import { SiteLayout } from "@/components/site/SiteLayout";
-
+const API_URL = import.meta.env.VITE_API_URL;
 export const Route = createFileRoute("/contact")({
   head: () => ({
     meta: [
@@ -39,7 +39,7 @@ function ContactPage() {
 };
 
   try {
-    const res = await fetch("http://127.0.0.1:8000/api/contact", {
+    const res = await fetch(`${API_URL}/api/contact`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
